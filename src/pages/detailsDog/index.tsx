d96ca047b633265edab59dd0d33d045dcb433179
VictorSalome <vsalome41@gmail.com>
2025-02-09 18:18:50 -0300

    feat: refatorar componente DetailsDog para usar Material-UI e melhorar a apresentação dos detalhes do pet

--- src/pages/detailsDog/index.tsx
+++ src/pages/detailsDog/index.tsx
@@ -1,12 +1,18 @@
 import { useParams } from "react-router-dom";
-import { useGetDogs } from "../../hooks/servicesApi/serviceHook";
+import { useGetDogs } from "../../hooks/serviceHook";
+import Avatar from '@mui/material/Avatar';
+import Typography from '@mui/material/Typography';
+import TextField from '@mui/material/TextField';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import Paper from '@mui/material/Paper';
 
 export const DetailsDog = () => {
   const { id } = useParams<{ id: string }>();
   const { data, error, isLoading } = useGetDogs();
 
   const dog = data?.dogs.find((dog) => dog._id === id);
-  console.log("dog", dog);
+
 
   if (isLoading) {
     return <div>Loading...</div>;
@@ -21,101 +27,76 @@ export const DetailsDog = () => {
   }
 
   return (
-    <div className="p-6 bg-gray-100 min-h-screen flex flex-col items-center">
-      <h1 className="text-2xl font-bold mb-4 text-center">Detalhes do pet</h1>
-      <div className="bg-white p-6 rounded-lg shadow-md w-full max-w-5xl flex ">
-        <div className="w-1/3 flex flex-col items-start">
-          <h1 className="text-3xl font-medium">{dog.name}</h1>
-          <img
-            src={dog.profileImage}
-            alt="avatar-dog"
-            className="w-72 h-72 rounded-md mr-4"
-          />
-        </div>
-        <div className="flex items-center justify-center">
-          <div className="flex flex-col">
-            <div className="flex items-center mb-2">
-              <label className="text-lg font-medium mr-2">Raça:</label>
-              <input
-                type="text"
-                id="name"
-                name="name"
+    <Container maxWidth="md">
+      <Paper elevation={3} sx={{ p: 4, mt: 4 }}>
+        <Box display="flex" gap={4}>
+          <Box display="flex" flexDirection="column" >
+            <Typography variant="h4" fontWeight={700} gutterBottom>
+              {dog.name.toUpperCase()}
+            </Typography>
+            <Avatar src={dog.profileImage} sx={{ width: 250, height: 250, mb: 2, borderRadius: 3 }} />
+          </Box>
+
+          <Box component="form" sx={{ display: 'flex', flexWrap: 'wrap', gap: 2 }}>
+            <Box sx={{ flex: '1 1 auto' }}>
+              <TextField
+                label="Raça"
                 value={dog.breed}
                 disabled
-                className="input-field px-4 py-2 border rounded-lg w-1/2"
+                sx={{ width: 'auto', flexShrink: 0 }}
               />
-            </div>
-            <div className="flex items-center">
-              <label className="text-lg font-medium mr-2">Sexo:</label>
-              <input
-                type="text"
-                id="name"
-                name="name"
+            </Box>
+            <Box sx={{ flex: '1 1 auto' }}>
+              <TextField
+                label="Sexo"
                 value={dog.gender}
                 disabled
-                className="input-field px-4 py-2 border rounded-lg w-1/2"
+                sx={{ width: 'auto', flexShrink: 0 }}
               />
-            </div>
-            <div className="flex items-center mt-2">
-              <label className="text-lg font-medium mr-2">Cor do pet:</label>
-              <input
-                type="text"
-                id="name"
-                name="name"
+            </Box>
+            <Box sx={{ flex: '1 1 auto' }}>
+              <TextField
+                label="Cor"
                 value={dog.color}
                 disabled
-                className="input-field px-4 py-2 border rounded-lg w-1/2"
+                sx={{ width: 'auto', flexShrink: 0 }}
               />
-            </div>
-            <div className="flex items-center mt-2">
-              <label className="text-lg font-medium mr-2">Dono do pet:</label>
-              <input
-                type="text"
-                id="name"
-                name="name"
+            </Box>
+            <Box sx={{ flex: '1 1 auto' }}>
+              <TextField
+                label="Tutor"
                 value={dog.ownerName}
                 disabled
-                className="input-field px-4 py-2 border rounded-lg w-1/2"
+                sx={{ width: 'auto', flexShrink: 0 }}
               />
-            </div>
-            <div className="flex items-center mt-2">
-              <label className="text-lg font-medium mr-2">Alimentação:</label>
-              <input
-                type="text"
-                id="name"
-                name="name"
-                value={dog.feeding.feedingFrequency}
-                disabled
-                className="input-field px-4 py-2 border rounded-lg w-1/2"
-              />
-            </div>
-            <div className="flex items-center mt-2">
-              <label className="text-lg font-medium mr-2">
-                Tipo do alimento:
-              </label>
-              <input
-                type="text"
-                id="name"
-                name="name"
+            </Box>
+            <Box sx={{ flex: '1 1 auto' }}>
+              <TextField
+                label="Alimentação"
                 value={dog.feeding.foodType}
                 disabled
-                className="input-field px-4 py-2 border rounded-lg w-1/2"
+                sx={{ width: 'auto', flexShrink: 0 }}
               />
-            </div>
-            <div className="flex items-center mt-2">
-              <label className="text-lg font-medium mr-2">Petisco:</label>
-              <input
-                type="text"
-                id="name"
-                name="name"
+            </Box>
+            <Box sx={{ flex: '1 1 auto' }}>
+              <TextField
+                label="Comida"
+                value={dog.feeding.feedingFrequency}
+                disabled
+                sx={{ width: 'auto', flexShrink: 0 }}
+              />
+            </Box>
+            <Box sx={{ flex: '1 1 auto' }}>
+              <TextField
+                label="Petisco"
                 value={dog.feeding.snackName ? dog.feeding.snackName : "Não"}
                 disabled
-                className="input-field px-4 py-2 border rounded-lg w-1/2"
+                sx={{ width: 'auto', flexShrink: 0 }}
               />
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+            </Box>
+          </Box>
+        </Box>
+      </Paper>
+    </Container>
   );
 };
