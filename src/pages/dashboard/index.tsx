a68efa25a7fd83ab058ea05abc30de0923c4d3b1
VictorSalome <vsalome41@gmail.com>
2025-02-14 16:53:02 -0300

    feat: adicionar componente HeaderDashboard à página inicial do dashboard

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,8 +1,13 @@
+import { HeaderDashboard } from "./components/Header";
 import HomeMain from "./homeMain";
+
+
+
 
 const HomePage = () => {
   return (
     <>
+      <HeaderDashboard />
       <HomeMain />
     </>
   );
