11a48b8ac6e461c1dcbb08c5a55b1a1d5d3adc3c
VictorSalome <vsalome41@gmail.com>
2025-02-14 08:36:49 -0300

    feat: atualizar rotas de autenticação para incluir prefixo 'auth' e corrigir importação do LoginPage

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Route, Routes, Outlet } from "react-router-dom";
 import Sidebar from './components/Sidebar';
 import MainContent from './pages/homePage/mainContent';
-import Login from './pages/auth/loginPage';
+import { LoginPage } from './pages/auth/loginPage';
 import Register from './pages/auth/registerPage';
 import HomePage from "./pages/homePage";
 import RegisterPet from "./pages/homePage/CadastroPet";
@@ -13,8 +13,8 @@ import { DetailsDog } from './pages/detailsDog';
 const AppRoutes: React.FC = () => {
     return (
         <Routes>
-            <Route path="/register" element={<Register />} />
-            <Route path="/login" element={<Login />} />
+            <Route path="auth/register" element={<Register />} />
+            <Route path="auth/login" element={<LoginPage />} />
             <Route path="/" element={<LayoutWithSidebar />}>
                 <Route index element={<MainContent><HomePage /></MainContent>} />
                 <Route path="cadastro-pet" element={<MainContent><RegisterPet /></MainContent>} />
