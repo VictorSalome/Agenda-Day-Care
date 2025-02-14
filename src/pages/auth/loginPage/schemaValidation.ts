4e6358605e7e0790428807afa59f9138a3b079ad
VictorSalome <vsalome41@gmail.com>
2025-02-14 08:37:36 -0300

    feat: adicionar validação de esquema para registro de usuários na página de login

---
+++ src/pages/auth/loginPage/schemaValidation.ts
@@ -0,0 +1,9 @@
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+
+const schema = yup.object().shape({
+  email: yup.string().email("E-mail inválido").required("E-mail é obrigatório"),
+  password: yup.string().required("Senha é obrigatória"),
+});
+
+export const schemaRegister = yupResolver(schema);
